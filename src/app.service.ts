623041a7eaa7367d5292999a1b40c5a39515ca02
Evan Bonertz <bonertz.unc@gmail.com>
2024-12-30 15:33:32 -0500

    add gemimni integration and env

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { GoogleGenerativeAI } from '@google/generative-ai';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Welcome to Nido!';
+  }
+
+  async getGeminiResponse(): Promise<string> {
+    const apiKey = process.env.GEMINI_API_KEY;
+    const genAI = new GoogleGenerativeAI(apiKey);
+
+    const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+
+    const prompt =
+      'Give me a response with the current version of Gemini and I interesting fact about Raleigh, North Carolina.';
+
+    const result = await model.generateContent(prompt);
+    console.log(result.response.text());
+    return result.response.text();
   }
 }
